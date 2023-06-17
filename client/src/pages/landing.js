f8561ec0d4008f78fc5d89b371e9bc63e94d291a
Jinan Lakan <lakanj25@gmail.com>
2023-06-17 20:21:08 +0200

    Create Landing Page

---
+++ client/src/pages/landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+    return (<div>
+        <p>Landing</p>
+    </div>)
+}
+
+export default Landing
