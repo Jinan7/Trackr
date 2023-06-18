f872409934931490b6a474f7ca74ba6563da0af9
Jinan Lakan <lakanj25@gmail.com>
2023-06-19 00:30:26 +0200

    Create Login Form

---
+++ client/src/pages/Register.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react'
+import {Logo, FormRow} from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+    name: '',
+    email: '',
+    password:'',
+    isMember: true,
+}
+
+const Register = () => {
+    const [values, setValues] = useState(initialState);
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
+
+    return (<Wrapper className='full-page'>
+        <form className='form' onSubmit={onSubmit}>
+            <Logo />
+            <h3>Login</h3>
+            <FormRow name='name' type='text' value={values.name} handleChange={handleChange}/>
+            <FormRow name='email' type='email' value={values.email} handleChange={handleChange}/>
+            <FormRow name='password' type='password' value={values.password} handleChange={handleChange}/>
+            <button type='submit' className='btn btn-hero'>Submit</button>
+        </form>
+    </Wrapper>)
+}
+
+export default Register;
