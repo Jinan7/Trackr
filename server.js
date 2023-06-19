64584803e25f3141f88a6d72ba45a0295e2f0a53
Jinan Lakan <lakanj25@gmail.com>
2023-06-19 23:53:58 +0200

    Create error handler

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 //middle ware
 import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 const app = express()
 
 
 
 app.get('/', (req, res) =>{
+    throw new Error("terhe")
     res.send('Welcome')
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
