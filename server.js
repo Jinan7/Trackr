6b160966ca8ddd1f02942a993c9d850548dae157
Jinan Lakan <lakanj25@gmail.com>
2023-06-19 23:43:52 +0200

    Create not found middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express'
+//middle ware
+import notFoundMiddleware from './middleware/not-found.js'
 
 const app = express()
+
+
 
 app.get('/', (req, res) =>{
     res.send('Welcome')
 })
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
