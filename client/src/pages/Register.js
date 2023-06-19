531a53f5f4f72d1cae4062f6d662033a8b98f20e
Jinan Lakan <lakanj25@gmail.com>
2023-06-19 21:29:03 +0200

    Add logic to display alert box

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import {Logo, FormRow, Alert} from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { useAppContext } from '../context/appContext'
 
 const initialState = {
     name: '',
@@ -12,23 +13,28 @@ const initialState = {
 
 const Register = () => {
     const [values, setValues] = useState(initialState);
-
+    const {isLoading, showAlert} = useAppContext()
     const toggleMember = () => {
         setValues({...values, isMember:!values.isMember})
     }
     const handleChange = (e) => {
-        console.log(e.target)
+        setValues({...values, [e.target.name]: e.target.value})
     }
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log(e.target)
+        const {name, email, password, isMember} = values
+        if(!email || !password || (!isMember && !name)){
+            //displayAlert()
+            return
+        }
+        console.log(values);
     }
 
     return (<Wrapper className='full-page'>
         <form className='form' onSubmit={onSubmit}>
             <Logo />
             <h3>{values.isMember ? "Login" : "Register"}</h3>
-            {values.showAlert && <Alert/>}
+            {showAlert && <Alert/>}
             {!values.isMember && (<FormRow name='name' type='text' value={values.name} handleChange={handleChange}/>)}
             <FormRow name='email' type='email' value={values.email} handleChange={handleChange}/>
             <FormRow name='password' type='password' value={values.password} handleChange={handleChange}/>
