c1bd110f9e09fc342643d2993d5310824b05cc35
Jinan Lakan <lakanj25@gmail.com>
2023-06-20 06:19:16 +0200

    Create router object

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,6 +1,7 @@
 import { get } from 'mongoose'
 import {createJob,deleteJob,showStats,updateJob,getAllJobs} from '../controllers/jobsController.js'
 import express from 'express'
+const router = express.Router()
 
 router.route('/').post(createJob).get(getAllJobs)
 router.route('/stats').post(createJob).get(getAllJobs)
