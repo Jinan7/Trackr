ac68340981d092992768c10ab4808c7b8abcc63a
Jinan Lakan <lakanj25@gmail.com>
2023-06-20 05:32:47 +0200

    Set up jobs router and controller

---
+++ routes/jobsRoutes.js
@@ -0,0 +1,8 @@
+import { get } from 'mongoose'
+import {createJob,deleteJob,showStats,updateJob,getAllJobs} from '../controllers/jobsController.js'
+import express from 'express'
+
+router.route('/').post(createJob).get(getAllJobs)
+router.route('/stats').post(createJob).get(getAllJobs)
+router.route('/:id').delete(deleteJob).patch(updateJob)
+export default router
