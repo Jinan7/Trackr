f22eeeca477c6dde512318f9e7bf2feaa63ef5fa
Jinan Lakan <lakanj25@gmail.com>
2023-06-20 06:18:38 +0200

    Creat user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, 
+    required: [true, 'Please provide name'], 
+    minlength:3, 
+    maxlength:2,
+    trim: true
+},
+    email: {type: String, 
+    required: [true, 'Please provide email'], 
+    validate:{
+        validator:validator.isEmail,
+        message:'Please provide a valid email'
+    },
+    unique: true,
+},
+    password: {type: String, 
+    required: [true, 'Please provide password'], 
+    minlength: 6,
+},
+    lastName: {type: String, 
+    trim:true, 
+    maxlength:20,
+    default: 'lastName'
+},
+})
+
+export default mongoose.model('User', UserSchema)
