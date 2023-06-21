5daaf6b5bf25bd127d378b9c3fbc37230a265700
Jinan Lakan <lakanj25@gmail.com>
2023-06-21 06:31:12 +0200

    Add toggle logout button functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,11 @@ import Wrapper from '../assets/wrappers/Navbar'
 import {FaAlignLeft, FaUserCircle, FaCaretDown} from 'react-icons/fa'
 import { useAppContext } from '../context/appContext'
 import Logo from './Logo'
+import { useState } from 'react'
+
 const Navbar = () => {
     const {toggleSidebar} = useAppContext()
+    const [showLogout, setShowLogout] = useState(false)
     return (<Wrapper>
         <div className="nav-center">
             <button type='button' className='toggle-btn' onClick={toggleSidebar}>
@@ -14,12 +17,12 @@ const Navbar = () => {
                 <h3 className='logo-text'>dashboard</h3>
             </div> 
             <div className="btn-container">
-                <button type='button' className='btn' onClick={()=>console.log('show/hide dropdown')}>
+                <button type='button' className='btn' onClick={() => setShowLogout(!showLogout)}>
                     <FaUserCircle/>
                     Jinan
                     <FaCaretDown/>
                 </button>
-                <div className="dropdown show-dropdown">
+                <div className={showLogout?"dropdown show-dropdown":"dropdown"}>
                   <button type="button" className='dropdown-btn' onClick={()=>console.log('logout user')}></button>  
                   logout
                 </div>
